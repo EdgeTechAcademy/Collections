e9e2e1aaf24ea07844201240083c24d8bc49a01d
Gary James <gjames@edgetechacademy.edu>
2020-04-27 06:49:06 -0500

    Add project files.

---
+++ Collections/Passenger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class Passenger
+    {
+        string LastName;
+        string FirstName;
+        float Age;
+        string paxClass;
+        bool isPassenger;
+        string Role;
+        bool Survivor;
+
+        public Passenger(string lastName, string firstName, float age, string paxClass, bool isPassenger, string role, bool survivor)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            Age = age;
+            this.paxClass = paxClass;
+            this.isPassenger = isPassenger;
+            Role = role;
+            Survivor = survivor;
+        }
+
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fileName = @"C:\Projects\csv\Titanic.csv";
+            // Example #1
+            // Read the file as one string.
+            //string text = System.IO.File.ReadAllText(fileName);
+
+            // Display the file contents to the console. Variable text is a string.
+            //System.Console.WriteLine("Contents of " + fileName + " = {0}", text);
+
+            // Example #2
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(fileName);
+
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of " + fileName + " = ");
+            Boolean firstLine = true;
+            List<Passenger> passengers = new List<Passenger>();
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+                if (firstLine)
+                {
+                    firstLine = false;
+                    continue;
+                }
+                string[] props = line.Split(",");
+                if (props[1].Length == 0) props[1] = "0";
+                string[] names = props[0].Split("/");
+                Passenger pax = new Passenger(names[0], names[1], float.Parse(props[1]), props[2], props[3].Equals("Passenger"), props[4], props[5].Equals("T"));
+                passengers.Add(pax);
+                // Use a tab to indent each line of the file.
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+    }
+}
